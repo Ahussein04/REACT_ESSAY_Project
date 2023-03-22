bf83980458436c34fa0be21009955906bfc8770c
Ritvik Thakur <ritvik.thakur04@gmail.com>
2023-03-21 22:20:30 -0400

    commit from ritz

--- components/DrawerNavigator.js
+++ components/DrawerNavigator.js
@@ -1,25 +1,25 @@
-import { createDrawerNavigator } from 'react-navigation-drawer';
-import { createAppContainer } from 'react-navigation';
+// import { createDrawerNavigator } from 'react-navigation-drawer';
+// import { createAppContainer } from 'react-navigation';
 
-import Home from './screens/Home';
-import Login from './screens/Login';
-import Essay from './screens/Essay';
-import TestScreen from './components/TestScreen';
+// import Home from './screens/Home';
+// import Login from './screens/Login';
+// import Essay from './screens/Essay';
+// import TestScreen from './components/TestScreen';
 
 
-const RootDrawerNavigator = createDrawerNavigator({
-    Home: {
-      screen: Home,
-    },
-    Login: {
-      screen: Login,
-    },
-    Essay: {
-        screen: Essay,
-    },
-    Test: {
-        screen: TestScreen,
-    }
-});
+// const RootDrawerNavigator = createDrawerNavigator({
+//     Home: {
+//       screen: Home,
+//     },
+//     Login: {
+//       screen: Login,
+//     },
+//     Essay: {
+//         screen: Essay,
+//     },
+//     Test: {
+//         screen: TestScreen,
+//     }
+// });
 
-export default createAppContainer(RootDrawerNavigator);
+// export default createAppContainer(RootDrawerNavigator);
--- constants/dummyData.js
+++ constants/dummyData.js
@@ -1,28 +1,34 @@
 //dummy data for testing the app
 
+import thumb from "../assets/thumb1.png";
+
 export const EssayData = [
     {
         id: "essay-01",
         name: "first essay",
         lastEdited: "1/28/23",
         wordCount: "103",      
+        pic: thumb,
     },
     {
         id: "essay-02",
         name: "dog died",
         lastEdited: "12/4/22",
-        wordCount: "57",      
+        wordCount: "57",   
+        pic: thumb,   
     },
     {
         id: "essay-03",
         name: "from future",
         lastEdited: "5/7/24",
-        wordCount: "432",      
+        wordCount: "432",   
+        pic: thumb,   
     },
     {
         id: "essay-04",
         name: "Tiger turner",
         lastEdited: "10/2/21",
-        wordCount: "550",      
+        wordCount: "550",
+        pic: thumb, 
     },
 ]
